134c5cd5e19fe99b64db97026bbd19954956583d
evanmhenry <evanmhenry@gmail.com>
2023-08-24 12:32:45 -0400

    completes frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,52 @@
-const App = () => <div>Hello World</div>
+import { useState } from 'react'
+import fetchWeather from './FetchWeather'
+import './App.css'
+import WeatherData from './WeatherData'
+
+const App = () => {
+	const [query, setQuery] = useState('')
+	const [weather, setWeather] = useState<WeatherData | undefined>()
+
+	const search = async (e: { key: string }) => {
+		if (e.key === 'Enter') {
+			const data = await fetchWeather(query)
+
+			setWeather(data)
+			setQuery('')
+		}
+	}
+
+	return (
+		<div className='main-container'>
+			<input
+				className='search'
+				type='text'
+				placeholder='Search...'
+				value={query}
+				onChange={(e) => setQuery(e.target.value)}
+				onKeyDown={(e) => {
+					if (e.key === 'Enter') {
+						search(e)
+					}
+				}}
+			/>
+			{weather?.main && (
+				<div className='city'>
+					<h2 className='city-name'>
+						<span>{weather.name}</span>
+						<sup>{weather.sys.country}</sup>
+					</h2>
+					<div className='city-temp'>
+						{Math.round(weather.main.temp)}
+						<sup>&deg;C</sup>
+					</div>
+					<div className='info'>
+						<img className='city-icon' src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+						<p>{weather.weather[0].description}</p>
+					</div>
+				</div>
+			)}
+		</div>
+	)
+}
 export default App
